4f4cb74d7d2dae07b8f653656c7c5580d890cea8
cn1109 <carlos.navarro.aburto@gmail.com>
2017-05-18 20:15:07 -0500

    Added Pig Latin

--- 02Week/PigLatin.cs
+++ 02Week/PigLatin.cs
@@ -1,17 +1,61 @@
 using System;
 
-public class Program
+namespace ConsoleApplication
 {
-    public static void Main()
+    public class PigLatin
     {
-        Console.WriteLine("What's the word?");
-        string word = Console.ReadLine();
-        Console.WriteLine(translateWord(word));
+        public void Run()
+        {
+            Console.WriteLine("What's the word?");
+            string word = Console.ReadLine();
+            Console.WriteLine(translateWord(word));
+        }
+
+        public static string translateWord(string word)
+        {
+            // (1) equal
+            // (2) hello
+
+            // Your code here
+            word = word.ToLower();
+            int vowelIndex = word.IndexOf('a'); //-1
+
+            //                 1 > -1           AND             1 < -1          OR            // -1 == -1  == TRUE
+            if( (word.IndexOf('e') > -1) &&  (word.IndexOf('e') < vowelIndex) || (vowelIndex == -1)  )
+            {
+                vowelIndex = word.IndexOf('e'); //1
+            }
+
+            //            -1 > -1       AND         -1 < 1             OR             1 == -1      == FALSE
+            if( (word.IndexOf('i') > -1 && word.IndexOf('i') < vowelIndex) || (vowelIndex == -1) ){
+                vowelIndex = word.IndexOf('i'); //-1
+            }
+
+            //              4 > -1       AND            4 < 1             OR             1 == -1    == FALSE
+            if ( (word.IndexOf('o') > -1 && word.IndexOf('o') < vowelIndex) || (vowelIndex == -1) ) {
+                vowelIndex = word.IndexOf('o'); //4
+            }
+
+            //            -1 > -1        AND          -1 < 1                OR    1 == -1          == FALSE
+            if ( (word.IndexOf('u') > -1 && word.IndexOf('u') < vowelIndex) || (vowelIndex == -1) ) {
+                vowelIndex = word.IndexOf('u');
+            }
+
+   
+            string firstPart = word.Substring(0, vowelIndex); // (0, 1)
+            string restWord = word.Substring(vowelIndex, word.Length - 1); // (1, 5-1)
+
+            // 1
+            Console.WriteLine("vowelIndex: " + vowelIndex); 
+
+            // (hello) substring(0,1) == h
+            Console.WriteLine("firstPart: " + firstPart);
+            Console.WriteLine("restWord: " + restWord);
+            Console.WriteLine(vowelIndex);
+
+
+            return "Pig Latin Word: " + restWord + firstPart + "ay"; 
+        }
     }
-    
-    public static string translateWord(string word)
-    {
-        // Your code here
-        return word;
-    }
+
 }
